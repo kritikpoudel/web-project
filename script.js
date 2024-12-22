6a9223e63595fe256c1fbf35db2e008f7f6ab800
kritik poudel <kritikpoudel808@gmail.com>
2024-12-22 14:44:09 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function showMenu() {
+  document.querySelector(".links").classList.toggle("show");
+}
+
+function myname() {
+  document.querySelector(".description h1").style.color = "red";
+}
+
+function validateForm(event) {
+  let email = document.querySelector("input[name='email']");
+  
+  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  let errors = [];
+  
+  if (!emailPattern.test(email.value)) {
+    errors.push("Please enter a valid email address");
+  }
+  
+  let p = document.getElementById("errors");
+  if (errors.length > 0) {
+    event.preventDefault();
+    p.innerHTML = errors.join('<br>');  
+  }
+}
